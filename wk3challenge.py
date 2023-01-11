5df2c104afd9d2fd9c6072badc8f1548f6a60c5a
Winnie Z <wz@Winnies-MacBook-Air.local>
2023-01-10 16:00:57 -0800

    firstcommit

---
+++ wk3challenge.py
@@ -0,0 +1,23 @@
+import os
+import csv
+
+PyBank_csv = "/Users/wz/Desktop/DA_Bootcamp/week3_Python/wk3challenge/PyBank/Resources/budget_data.csv"
+
+with open (PyBank_csv, "r") as read:
+    csv_read = csv.reader(read, delimiter=",")
+
+
+print ("Financial Analysis")
+print ('-------------------------------------')
+
+# total number of months in the dataset
+
+
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in profits (date and amount) over the entire period
